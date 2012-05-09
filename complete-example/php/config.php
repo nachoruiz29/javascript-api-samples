435e8b05fb36c8a926279b8ec100e3604b1091a8
jaumejane <jaume.jase@gmail.com>
2012-05-09 12:21:40 +0300

    Update complete-example/php/config.php

--- complete-example/php/config.php
+++ complete-example/php/config.php
@@ -3,9 +3,9 @@
 	$protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE ? 'http' : 'https';
 	define( "OAUTH_HOST", $protocol . "://" . $_SERVER['HTTP_HOST'] );
 
-	// ACCESS ZYNCRO API INT
-	define("CONSUMER_KEY", "e0ad8093cdbe4407a93a7cd256ef0034");
-	define("CONSUMER_SECRET", "af186398d4d44cf2803f29eb08273b0f");
+	// ACCESS ZYNCRO API
+	define("CONSUMER_KEY", "CONSUMER_KEY");
+	define("CONSUMER_SECRET", "CONSUMER_SECRET");
 	define("REQUEST_TOKEN_URL", OAUTH_HOST . "/tokenservice/oauth/v1/get_request_token");
 	define("ACCESS_TOKEN_URL", OAUTH_HOST . "/tokenservice/oauth/v1/get_access_token");
 	
